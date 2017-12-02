18e7dd4d8d567123612bbaf2dd8dc419d0b74678
Dustin Specker <DustinSpecker@DustinSpecker.com>
2017-12-02 16:10:54 -0600

    add day2b

--- day02/run.js
+++ day02/run.js
@@ -9,6 +9,16 @@ const converRowsToCells = row =>
 const calcRowDiff = cells =>
   cells[cells.length - 1] - cells[0]
 
+const calcRowEvenDivide = cells => {
+  for (let i = 0; i < cells.length; i++) {
+    for (let j = i + 1; j < cells.length; j++) {
+      if (cells[j] % cells[i] === 0) {
+        return cells[j] / cells[i]
+      }
+    }
+  }
+}
+
 const calcChecksum = (spreadsheet, calcRowMethod) =>
   spreadsheet
     .split('\n')
@@ -24,4 +34,5 @@ fs.readFile('input.txt', (err, data) => {
   const spreadsheet = data.toString().trim()
 
   console.log('2a', calcChecksum(spreadsheet, calcRowDiff))
+  console.log('2b', calcChecksum(spreadsheet, calcRowEvenDivide))
 })
