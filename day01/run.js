c54a6da067a6a9ae5035925a9c8736c1f2bdeb96
Dustin Specker <DustinSpecker@DustinSpecker.com>
2017-12-02 15:43:31 -0600

    add day1b

--- day01/run.js
+++ day01/run.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-const solveCaptcha = str =>
+const solveCaptcha = (str, lookForward = 1) =>
   str
     .split('')
     .map(s => parseInt(s, 10))
-    .filter((num, index, array) => num === array[index % array.length])
+    .filter((num, index, array) => num === array[(index + lookForward) % array.length])
     .reduce((total, cur) => total + cur)
 
 fs.readFile('input.txt', (err, data) => {
@@ -15,4 +15,5 @@ fs.readFile('input.txt', (err, data) => {
   const numbersStr = data.toString().trim()
 
   console.log('1a', solveCaptcha(numbersStr))
+  console.log('1b', solveCaptcha(numbersStr, numbersStr.length / 2))
 })
